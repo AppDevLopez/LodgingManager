b5b0f26addfd1863b875a10c40a2fa09c1bf2c1a
Luis Lopez <luis.lopez@sigma.se>
2016-05-09 19:00:27 +0200

    Test

---
+++ LodgingManager/LodgingManager/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="LodgingManager.MvcApplication" Language="C#" %>
---
+++ LodgingManager/LodgingManager/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LodgingManager.Startup))]
+namespace LodgingManager
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
